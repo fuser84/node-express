e363bac090956402f0fd9d41a5f87108d15bec31
fuser <vnovodid@gmail.com>
2018-03-06 22:16:11 +0200

    Express Serve Static Files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+
 
 const hostname = 'localhost';
 const port = 3000;
@@ -7,9 +9,14 @@ const port = 3000;
 //our application will be using express
 const app = express();
 
+//add additional info into the console due to dev parameter
+app.use(morgan('dev'));
+
+//serve the *.html from public folder
+app.use(express.static(__dirname+  '/public'));
+
 //method with function which will help to set up server
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end(`<html><body><h1>This is an Express Server</h1></body></html>`);
